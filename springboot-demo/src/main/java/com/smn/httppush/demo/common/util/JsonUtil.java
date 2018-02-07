9e164888d9290e1dcd1e3273d047f92f56c1f793
breakawayz <375831500@qq.com>
2018-02-07 17:47:30 +0800

    copyright

--- springboot-demo/src/main/java/com/smn/httppush/demo/common/util/JsonUtil.java
+++ springboot-demo/src/main/java/com/smn/httppush/demo/common/util/JsonUtil.java
@@ -1,9 +1,20 @@
+/*
+ * Copyright (C) 2018. Huawei Technologies Co., LTD. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of Apache License, Version 2.0.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Apache License, Version 2.0 for more details.
+ */
 package com.smn.httppush.demo.common.util;
 
-        import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-        import java.util.HashMap;
-        import java.util.Map;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * json util
